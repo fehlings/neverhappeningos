0ddbda37f4e85e249fb157c73a3b4bdeacfc5b10
Luca Fehlings <fehlingsl@gmx.de>
2021-12-12 14:20:57 +0100

    syntax

--- kernel/low_level.c
+++ kernel/low_level.c
@@ -1,7 +1,7 @@
 unsigned char port_byte_in(unsigned short port) {
     // Reads byte from specified port
     unsigned char result;
-    __asm__("in %%dx, %%al" : "=a" (result) : "d") (port));
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
 
@@ -12,12 +12,12 @@ void port_byte_out(unsigned short port, unsigned char data) {
 
 unsigned short port_word_in(unsigned short port) {
     // Reads word from specified port
-    unsigned short result
-    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    unsigned short result;
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
 
 void port_word_out(unsigned short port , unsigned short data) {
     // Writes word to specified port
-    __asm__("out %%ax, %%dx" : :"a" (data), "d" (port)):
+    __asm__("out %%ax, %%dx" : :"a" (data), "d" (port));
 }
